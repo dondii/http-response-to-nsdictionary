eaf62d9e0309d4ca92bea12a861d5d3258a6e063
Dondi Sasmita <dondi.sasmita@gmail.com>
2012-10-23 12:20:24 +0800

    add view and put the url inside view controller. later should create those in different class inherit from BaseHTTPAPI

--- Greyjoy/Greyjoy/ViewController.h
+++ Greyjoy/Greyjoy/ViewController.h
@@ -7,7 +7,24 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "BaseHttpAPI.h"
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <BaseHttpAPIDelegate>
+{
+    @private
+    UITextField *urlField_;
+    UIButton *submitBtn_;
+    UITextView *resultTextView_;
+    UISegmentedControl *respFormatSegment_;
+    
+    BaseHttpAPI *baseHttpApi_;
+}
+
+@property (nonatomic, strong) IBOutlet UITextField *urlField;
+@property (nonatomic, strong) IBOutlet UIButton *submitBtn;
+@property (nonatomic, strong) IBOutlet UITextView *resultTextView;
+@property (nonatomic ,strong) IBOutlet UISegmentedControl *respFormatSegment;
+
+- (IBAction)submitBtnDidTap:(id)sender;
 
 @end
--- Greyjoy/Greyjoy/ViewController.m
+++ Greyjoy/Greyjoy/ViewController.m
@@ -14,6 +14,9 @@
 
 @implementation ViewController
 
+@synthesize urlField = urlField_, submitBtn = submitBtn_,
+resultTextView = resultTextView_, respFormatSegment = respFormatSegment_;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -24,6 +27,41 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)submitBtnDidTap:(id)sender{
+    BaseAPIResponseFormat baseAPIFormat;
+    switch (respFormatSegment_.selectedSegmentIndex) {
+        case 0:
+            baseAPIFormat = baseAPIFormatJSON;
+            break;
+            
+        case 1:
+            baseAPIFormat = baseAPIFormatXML;
+            break;
+    }
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc]init];
+    [request setURL:[NSURL URLWithString:urlField_.text]];
+    [request setHTTPMethod:@"GET"];
+    
+    NSString *header = [NSString stringWithFormat:@"application/vnd.bhg_rewards.v1"];
+    [request setValue:header forHTTPHeaderField:@"Accept"];
+    
+    baseHttpApi_ = [[BaseHttpAPI alloc] initWithDictionary:nil
+                                                  delegate:self];
+    [baseHttpApi_ callAPI:request expectResponse:baseAPIFormat];
+}
+
+#pragma mark -
+#pragma mark BaseHTTPAPI Delegate
+
+- (void)baseHttpAPIDidReturnSuccessfully:(NSDictionary *)result{
+    NSLog(@"%@",result);
+    
+}
+
+- (void)baseHttpAPIDidReturnWithError:(NSError *)error{
+    
 }
 
 @end
